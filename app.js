09881f33f661866914ae24af48dbc23060cf5d9e
Suzy Burgum <suzyburgum@gmail.com>
2025-03-19 14:11:13 -0400

    add main application file with API routes and server setup

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import { PORT } from "./config/env.js";
+
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
+const app = express();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API running on http://localhost:${PORT}`);
+});
+
+export default app;
