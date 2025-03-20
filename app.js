00bed511dfd58af1f723c69c3892876f70f9c9a5
Suzy Burgum <suzyburgum@gmail.com>
2025-03-20 14:47:41 -0400

    add arcjet middleware to app for enhanced request handling

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./databse/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
-
-// built-in/suggested middleware 
+// built-in/suggested middleware
 // handle json data sent in requrests/API calls
 app.use(express.json());
 // helps process the form data sent via  HTML forms in a simple format
@@ -20,18 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 // reads cookies from incoming request so app can store data.
 app.use(cookieParser());
 
-
+app.use(arcjetMiddleware);
 
 // Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-
 // custom middleware
 app.use(errorMiddleware);
-
-
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
